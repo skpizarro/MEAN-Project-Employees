a14bc70f9459328c3a952b68dca664ecafc1e26b
Juan Pablo Pizarro <juanpablopizarro97@gmail.com>
2020-11-04 23:43:08 -0500

    Se crea un CRUD básico en la  API REST

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const app = express();
 app.set('port',process.env.PORT || 3000)
 
 app.use(morgan('dev'));
+app.use(express.json());// objetos JSON que lleguen desde un content-type
+app.use(express.urlencoded({extended:false})) // para entender datos que vengan desde un formulario
 
 app.use('/api/employees',require('./routes/employees.routes'));
 
--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,14 +1,95 @@
 const employeeCtrl ={}
 
-employeeCtrl.getEmployees = (req,res)=>{
-    res.send('Get employees')
-}
-employeeCtrl.createEmployee = (req,res)=>{
-    res.send('Create employees')
-}
-employeeCtrl.getEmployee = (req,res)=>{}
-employeeCtrl.editEmployee = (req,res)=>{}
-employeeCtrl.deleteEmployee = (req,res)=>{}
+const Employee = require('../models/Employee');
 
+employeeCtrl.getEmployees = async(req,res)=>{
+    const employees = await Employee.find();
+    res.status(200).json({
+        ok:true,
+        employees
+    })
+}
+
+
+
+employeeCtrl.createEmployee = async(req,res)=>{
+    try{
+        const {name,position,office,salary} = req.body;
+        const newEmployee = new Employee({name,position,office,salary});
+        await newEmployee.save();
+        res.status(200).json({
+            ok:true,
+            message: "Employee created",
+        })
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err500)
+    }
+}
+employeeCtrl.getEmployee = async (req,res)=>{
+    try{
+        const {id}=req.params;
+        const employeeDb= await Employee.findById(id);
+        res.status(200).json({
+            ok:true,
+            employee:employeeDb
+        })
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err500)
+    }
+}
+
+employeeCtrl.editEmployee = async (req,res)=>{
+    try{
+        const {id}=req.params;
+        const employeeUpdated = await Employee.findByIdAndUpdate(id,req.body)
+
+        if(!employeeUpdated){
+            return res.status(400).json({
+                ok:true,
+                message:"Employee does´n exist"
+            })
+        }
+        res.status(200).json({
+            ok:true,
+            message:"Employee was updated"
+        })
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err500);
+    }
+}
+
+employeeCtrl.deleteEmployee = async(req,res)=>{
+    try{
+        const {id}= req.params;
+        const employeeDeleted = await Employee.findByIdAndDelete(id);
+        console.log(employeeDeleted);
+        if(!employeeDeleted){
+            return res.status(400).json({
+                ok:true,
+                message:"Employee does´n exist"
+            })
+        }
+
+        res.status(200).json({
+            ok:true,
+            message:"Employee was deleted"
+        })
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err500)
+    }
+
+
+}
+
+const err500 ={
+    ok:false,
+    message:"Ocurrió un problema"
+}
 
 module.exports= employeeCtrl;
